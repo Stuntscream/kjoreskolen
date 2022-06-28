ba661e1aa046e338d136b118ca3fd73cb73a2d46
Stuntscream <107320869+Stuntscream@users.noreply.github.com>
2022-06-28 21:07:18 +0200

    sortrng + js lagt inn i opplæring, bildr funkr ike

---
+++ js/Omtaler.js
@@ -0,0 +1,9 @@
+function Omtaler() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        x.style.display == "block";
+    } else {
+        x.style.display = "none";
+    }
+}
