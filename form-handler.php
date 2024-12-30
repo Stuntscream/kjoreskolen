43132c8c0fdb67d4619db23c8825432485206bff
Stuntscream <107320869+Stuntscream@users.noreply.github.com>
2024-12-30 19:25:10 +0100

    Maaange endirnger

--- form-handler.php
+++ form-handler.php
@@ -1,27 +1,38 @@
 <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
+
+$phone_number = $_POST['phone_number'];
 $message = $_POST['message'];
+$birth_number = $_POST['birth_number'];
 
-$email_from = 'ower@ower.no';
+// Valider fødselsnummer (f.eks. at det har 11 siffer)
+if (!preg_match("/^[0-9]{11}$/", $birth_number)) {
+    die("Ugyldig fødselsnummer. Fødselsnummer må inneholde 11 siffer.");
+}
 
-$email_subject = 'Testing av formhandler';
+// Valider e-postadresse
+if (!filter_var($visitor_email, FILTER_VALIDATE_EMAIL)) {
+    die("Ugyldig e-postadresse.");
+}
+
+$email_from = $visitor_email;
+
+$email_subject = 'Henvendelse - Form handler';
 
 $email_body = "User Name: $name.\n".
               "User Email: $visitor_email.\n".
-              "Subject: $subject.\n".
+              "Fødselsnummer: $birth_number.\n".
+              "Telefonnummer: $phone_number.\n".
               "User message: $message .\n";
-
 
 $to = "kontakt@ower.no";
 
 $headers = "From: $email_from \r\n";
-
 $headers .= "Reply-To: $visitor_email \r\n";
 
-mail ($to,$email_subject,$email_body,$headers);
+mail ($to, $email_subject, $email_body, $headers);
 
 header ("Location: kontakt.html");
 
-?>
+?> 
